49ab2147c565a88106673ddcb52e7db435e32da2
vazgiovanna <giovannaaguiarvaz@gmail.com>
2024-11-14 17:06:39 -0300

    Atividade

---
+++ Html e Css/Atividades/Aula1Js/scripts/somar.js
@@ -0,0 +1,11 @@
+var n1 = prompt("Digite o primeiro número: ")
+var n2 = prompt("Digite o segundo número: ")
+
+
+// number() transforma texto em número
+var soma = Number(n1) + Number(n2)
+alert("A soma dos números é de: "+ soma)
+
+var variavel_global = 1
+let variavel_local = 3
+const variavel_constante = "fixo"
---
+++ Html e Css/Atividades/Aula1Js/scripts/teste.js
@@ -0,0 +1,8 @@
+
+var nome = prompt("Qual o seu nome?")
+var numero_fav = 11
+var ligado = false
+var valor_total = 1.7
+var lista = []
+var aniversario = prompt('Qual o dia do seu aniversário?')
+alert('Olá '+ nome +', o dia do seu aniversário é '+ aniversario+ '!')
