107d896e3e240442c5979153a4b9e87ea171d2a4
vazgiovanna <giovannaaguiarvaz@gmail.com>
2024-11-14 17:41:14 -0300

    Atividade

---
+++ Html e Css/Atividades/Script1/scripts/index.js
@@ -0,0 +1,5 @@
+var numClientes = prompt("Insira a quantidade de clientes: ")
+var valorTotal = prompt("Insira o valor da comanda: ")
+
+var valorFinal = Number(valorTotal) / Number(numClientes)
+alert("O valor para cada cliente ficou de: "+valorFinal)
---
+++ Html e Css/Atividades/Script2/scripts/index.js
@@ -0,0 +1,5 @@
+var num = prompt("Insira um número para saber seu sucessor e antecessor: ")
+var suc = Number(num) + 1
+var ant = Number(num) - 1
+
+alert("O antecessor de "+num+" é "+ant+" e o sucessor é "+suc)
