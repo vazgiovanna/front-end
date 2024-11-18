db04412ac30b592ed7b621cf0a841d59048fa87e
vazgiovanna <giovannaaguiarvaz@gmail.com>
2024-11-18 17:46:21 -0300

    Atividade

---
+++ Html e Css/Atividades/Aula2Js/scripts/script.js
@@ -0,0 +1,16 @@
+function explicacao1(){
+    if (false){
+        console.log("Fiz o se!")
+    }else{
+        console.log("senao")
+    }
+    alert("Um código")
+}
+
+function explicacao2(){
+    alert("Um código bem daora")
+}
+
+function explicacao3(){
+    alert("Um código mais daora ainda")
+}
