2b35025f648bfbb17e93a26e5661851659d5ae56
vazgiovanna <giovannaaguiarvaz@gmail.com>
2024-11-22 17:42:19 -0300

    Atividade

--- Html e Css/Atividades/While/scripts/index.js
+++ Html e Css/Atividades/While/scripts/index.js
@@ -11,16 +11,25 @@ function testes(){
 //testes()
 
 function carregarCatalogo(){
-    for(let i = 0; i < 4; i++){
+    var livros =[
+        ['images/livrodeclinio.jpg', 'Declínio de um homem'],
+        ['images/noitesbrancas.jpg', 'Noites brancas'],
+        ['images/evelynhugo.jpg', 'Os 7 maridos de Evelyn Hugo'],
+        ['images/conde.jpg', 'O conde de monte cristo'],
+        ['images/crimecastigo.jpg', 'Crime e castigo'],
+        ['images/miseraveis.jpg', 'Os miseráveis'],
+    ]
+
+    livros.forEach(cadaLivro => {
         document.getElementById('catalogo').innerHTML +=`
         <div class="livro">
-                <img src="/images/livrodeclinio.jpg">
-                <h4>Declínio de um homem</h4>
+                <img src="${cadaLivro[0]}">
+                <h4>${cadaLivro[1]}</h4>
                 <button>Adicionar
                     <img src="/images/plus-circle.svg">
                 </button>
             </div>`
-    }
+    })
 }
 
 carregarCatalogo()
