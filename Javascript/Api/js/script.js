31cd059882b4ae122e2ea71aa934af78f9056234
vazgiovanna <giovannaaguiarvaz@gmail.com>
2025-06-16 17:45:45 -0300

    Revisão API

---
+++ Javascript/Api/js/script.js
@@ -0,0 +1,21 @@
+api = "https://pokeapi.co/api/v2/";
+endpoint = "pokemon";
+
+consulta = api + endpoint;
+
+fetch(consulta).then((resposta) => resposta.json()).then((dados) => {
+ // console.log(dados);
+dados.results.map((infoPokemon) => {
+    // console.log(infoPokemon)
+    // listar informações de cada pokemon
+    fetch(infoPokemon.url).then((resposta) => resposta.json()).then((dadosDeCadaPoke) =>{
+        console.log(dadosDeCadaPoke);
+        document.querySelector("#pokemons").innerHTML += `
+            <div class="pokemon card p-4 d-flex align-items-center">
+                <img src="${dadosDeCadaPoke.sprites.front_default}"
+                <h4>${dadosDeCadaPoke.name}</h4>
+            </div>
+        `
+    })
+    })
+})
